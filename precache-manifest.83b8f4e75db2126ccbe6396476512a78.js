7d726d20dd51986da8fa96ebcdf708e706f52988
Peter Tshiani <tshianipeter@gmail.com>
2019-10-16 14:02:47 +0200

    Updates

---
+++ precache-manifest.83b8f4e75db2126ccbe6396476512a78.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60cac928e0a3aa64a4b93d74592465cc",
+    "url": "/component-hidemenu-bar/index.html"
+  },
+  {
+    "revision": "b499883318b6116fedb2",
+    "url": "/component-hidemenu-bar/static/css/main.028281f1.chunk.css"
+  },
+  {
+    "revision": "e1a26c204018679a1f9a",
+    "url": "/component-hidemenu-bar/static/js/2.20ecc64c.chunk.js"
+  },
+  {
+    "revision": "b499883318b6116fedb2",
+    "url": "/component-hidemenu-bar/static/js/main.ef7c3ae3.chunk.js"
+  },
+  {
+    "revision": "e2e12282c3dbaeed4cc2",
+    "url": "/component-hidemenu-bar/static/js/runtime~main.cde6f9f9.js"
+  },
+  {
+    "revision": "497c2d6697eda1282bd50f2b7f544e8f",
+    "url": "/component-hidemenu-bar/static/media/2.497c2d66.jpeg"
+  },
+  {
+    "revision": "1d0c0b75b19e07801a19df625c1798cf",
+    "url": "/component-hidemenu-bar/static/media/image1.1d0c0b75.JPG"
+  },
+  {
+    "revision": "982dffaa0051966698b78eacf3631a09",
+    "url": "/component-hidemenu-bar/static/media/logo1.982dffaa.png"
+  },
+  {
+    "revision": "0b7c1d8df10c764b42cfedbc8cf98484",
+    "url": "/component-hidemenu-bar/static/media/promotion2019.0b7c1d8d.jpg"
+  },
+  {
+    "revision": "569bea4da47621f27ca2d7781351eeab",
+    "url": "/component-hidemenu-bar/static/media/s1_16.569bea4d.jpg"
+  },
+  {
+    "revision": "93dab6b0b5282175ac14cbec504230e1",
+    "url": "/component-hidemenu-bar/static/media/s2.93dab6b0.jpg"
+  },
+  {
+    "revision": "616bc27cf10e37323631231e614fff8c",
+    "url": "/component-hidemenu-bar/static/media/v3.616bc27c.jpg"
+  },
+  {
+    "revision": "6474f415f29c40c5a174945e86851f10",
+    "url": "/component-hidemenu-bar/static/media/wed.6474f415.JPG"
+  }
+]);
